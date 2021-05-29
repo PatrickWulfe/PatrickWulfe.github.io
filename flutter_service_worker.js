c4391138242ee21a65a992c8095eef17fe47d880
PatrickWulfe <wulfep@gmail.com>
2021-05-28 19:49:59 -0700

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,8 +11,8 @@ const RESOURCES = {
 "icons/favicon.png": "fa7fde306a35e196516bf8b1d6100f14",
 "icons/Icon-192.png": "8afa706053b186edf013b6c9880872f9",
 "icons/Icon-512.png": "70dd4c7d62910514def11f21e23d99f8",
-"index.html": "ebd09dc4d53c58f1c9a4ff9251f883ee",
-"/": "ebd09dc4d53c58f1c9a4ff9251f883ee",
+"index.html": "4ab3d3c9fd6a9ab87e8638da00cd9b01",
+"/": "4ab3d3c9fd6a9ab87e8638da00cd9b01",
 "main.dart.js": "7b22e371e869cca0280cdd6c9cd4322b",
 "manifest.json": "ef22af793dc356468ea258e68a06f25c",
 "version.json": "0323328b10ae5e701c91561f7e8a8f65"
