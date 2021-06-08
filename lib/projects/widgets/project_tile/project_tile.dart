1db1ea51be9f63ac0fc98280bd6def0206ab1442
Patrick Wulfe <wulfep@gmail.com>
2021-06-08 15:12:16 -0700

    refactoring

--- lib/projects/widgets/project_tile/project_tile.dart
+++ lib/projects/widgets/project_tile/project_tile.dart
@@ -1,75 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio_webapp/projects/projects.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ProjectTile extends StatelessWidget {
-  const ProjectTile(
-      {Key? key,
-      required this.title,
-      required this.description,
-      required this.url})
-      : super(key: key);
+  const ProjectTile({
+    Key? key,
+    required this.info,
+  }) : super(key: key);
 
-  final String title;
-  final String description;
-  final String url;
+  final ProjectTileInfo info;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: _handleTap,
-      child: Card(
-        color: Colors.white10,
-        child: Container(
-          padding: const EdgeInsets.all(12),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.white12,
-              width: 3,
+    return FractionallySizedBox(
+      widthFactor: 2 / 3,
+      child: GestureDetector(
+        onTap: _handleTap,
+        child: Card(
+          color: Colors.transparent,
+          child: Container(
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.white12,
+                width: 3,
+              ),
             ),
-          ),
-          child: Column(
-            children: [
-              Card(
-                color: Colors.black12,
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      title,
-                      style: Theme.of(context).textTheme.headline5,
+            child: Column(
+              children: [
+                Card(
+                  color: Colors.transparent,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        info.title,
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: Card(
-                  color: Colors.black12,
-                  child: Padding(
-                    padding: const EdgeInsets.all(30.0),
-                    child: Column(
-                      children: [
-                        Column(
-                          children: [
-                            const Image(image: AssetImage('images/nyan.gif')),
-                            Text(
-                              'Nyan cat as a placeholder until I figure out what I want here',
-                              style: Theme.of(context).textTheme.caption,
-                            ),
-                          ],
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            description,
-                            style: Theme.of(context).textTheme.bodyText1,
+                Expanded(
+                  child: Card(
+                    color: Colors.transparent,
+                    child: Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: Column(
+                        children: [
+                          FractionallySizedBox(
+                            widthFactor: 4 / 5,
+                            child: Image(image: AssetImage(info.image)),
                           ),
-                        ),
-                      ],
+                          Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              info.description,
+                              style: Theme.of(context).textTheme.bodyText1,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
@@ -77,8 +72,8 @@ class ProjectTile extends StatelessWidget {
   }
 
   void _handleTap() async {
-    await canLaunch(url)
-        ? await launch(url)
-        : throw Exception('Could not launch $url');
+    await canLaunch(info.url)
+        ? await launch(info.url)
+        : throw Exception('Could not launch ${info.url}');
   }
 }
