57b953950d44db32c52883130d34eac700c813cc
Patrick Wulfe <wulfep@gmail.com>
2023-04-30 11:40:02 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.portfolio_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:portfolio_project/app/app.dart';
+import 'package:portfolio_project/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
