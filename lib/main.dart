ba4ea08737363237a92f652a54f230e2bb8939b7
Patrick Wulfe <wulfep@gmail.com>
2023-05-03 13:27:13 -0700

    changed env yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:portfolio_project/bootstrap.dart';
 import 'package:portfolio_project/src/app/app_index.dart';
 
 void main() async {
-  await dotenv.load();
+  await dotenv.load(mergeWith: Platform.environment);
   await bootstrap(() => const App());
 }
