03785f6429d7828c2f05b4698373a35aae05590c
Patrick Wulfe <wulfep@gmail.com>
2023-05-03 13:41:17 -0700

    removed platform environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'dart:io';
-
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:portfolio_project/bootstrap.dart';
 import 'package:portfolio_project/src/app/app_index.dart';
 
 void main() async {
-  await dotenv.load(mergeWith: Platform.environment);
+  await dotenv.load();
   await bootstrap(() => const App());
 }
