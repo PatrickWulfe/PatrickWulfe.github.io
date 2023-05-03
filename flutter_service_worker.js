bfbcbbc6c21e741dfdd9952a423c8daa0a460cab
Patrick Wulfe <wulfep@gmail.com>
2023-05-03 15:14:15 -0700

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,8 +32,8 @@ const RESOURCES = {
 "icons/favicon.png": "7a26ea9d4e61122fc023b84e5acdb148",
 "icons/Icon-192.png": "12d05db1c0b26d429b54e3f8c946a496",
 "icons/Icon-512.png": "aabba36693aba0ec8d954adf297d3125",
-"index.html": "f02bd98ee431a981e22e0b5de2709ab0",
-"/": "f02bd98ee431a981e22e0b5de2709ab0",
+"index.html": "3121ef830a6151f707811d9fe7270015",
+"/": "3121ef830a6151f707811d9fe7270015",
 "main.dart.js": "dafcb090ab7d69c2277e2232e811bf0e",
 "manifest.json": "8f5426026303f2d7239f1a6aae20eb40",
 "version.json": "5cc9695ee3e8c1f86234c524152ea8f8"
